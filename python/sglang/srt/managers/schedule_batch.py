4c5ab61d338e68f28dd6b0b16f992f04d86a44f9
liuchuan <925992278@qq.com>
2026-01-28 12:06:40 +0000

    [Perf] Opt get_page_table & stage1

--- python/sglang/srt/managers/schedule_batch.py
+++ python/sglang/srt/managers/schedule_batch.py
@@ -1553,6 +1553,8 @@ class ScheduleBatch(ScheduleBatchDisaggregationDecodeMixin):
     cu_new_compress_k2_token_nums_cpu: Optional[torch.Tensor] = None
     total_compress_k2_token_nums_cpu: Optional[torch.Tensor] = None
     cu_total_compress_k2_token_nums_cpu: Optional[torch.Tensor] = None
+    
+    cache_seqlens_int32_stage1_cpu: Optional[torch.Tensor] = None
 
     @classmethod
     def init_new(
@@ -2496,6 +2498,7 @@ class ScheduleBatch(ScheduleBatchDisaggregationDecodeMixin):
             )
             
             self.sparse_cache_seqlens_int32_cpu = torch.repeat_interleave(sparse_cache_seqlens_cpu, 2)
+            self.cache_seqlens_int32_stage1_cpu = self.seq_lens_cpu - 1
             
             self.sparse_cu_seqlens_k_cpu = F.pad(torch.cumsum(self.sparse_cache_seqlens_int32_cpu, dim=0, dtype=torch.int32), (1, 0))
             
@@ -2777,7 +2780,7 @@ class ScheduleBatch(ScheduleBatchDisaggregationDecodeMixin):
             cu_new_compress_k2_token_nums_cpu=self.cu_new_compress_k2_token_nums_cpu,
             total_compress_k2_token_nums_cpu=self.total_compress_k2_token_nums_cpu,
             cu_total_compress_k2_token_nums_cpu=self.cu_total_compress_k2_token_nums_cpu,
-            
+            cache_seqlens_int32_stage1_cpu=self.cache_seqlens_int32_stage1_cpu,
         )
 
     def copy(self):
@@ -3049,3 +3052,4 @@ class ModelWorkerBatch:
     
     total_compress_k2_token_nums_cpu: Optional[torch.Tensor] = None
     cu_total_compress_k2_token_nums_cpu: Optional[torch.Tensor] = None
+    cache_seqlens_int32_stage1_cpu: Optional[torch.Tensor] = None
