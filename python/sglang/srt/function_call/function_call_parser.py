11f31f7fc5dfd0effee9b96a42f9ac342df210d9
Jiayi Li <hyleepp@gmail.com>
2026-03-02 14:49:36 +0800

    [update] update tool use

--- python/sglang/srt/function_call/function_call_parser.py
+++ python/sglang/srt/function_call/function_call_parser.py
@@ -29,6 +29,7 @@ from sglang.srt.function_call.lfm2_detector import Lfm2Detector
 from sglang.srt.function_call.llama32_detector import Llama32Detector
 from sglang.srt.function_call.mimo_detector import MiMoDetector
 from sglang.srt.function_call.minimax_m2 import MinimaxM2Detector
+from sglang.srt.function_call.minicpm4_xml_detector import MiniCPM4XmlFormatDetector
 from sglang.srt.function_call.mistral_detector import MistralDetector
 from sglang.srt.function_call.poolside_v1_detector import PoolsideV1Detector
 from sglang.srt.function_call.pythonic_detector import PythonicDetector
@@ -76,6 +77,7 @@ class FunctionCallParser:
         "step3p5": Qwen3CoderDetector,
         "minimax-m2": MinimaxM2Detector,
         "trinity": TrinityDetector,
+        "minicpm4_xml": MiniCPM4XmlFormatDetector,
         "interns1": InternlmDetector,
         "hermes": HermesDetector,
         "hunyuan": HunyuanDetector,
